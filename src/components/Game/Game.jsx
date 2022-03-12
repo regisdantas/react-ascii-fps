2c2315a4c260dd6a09ae8d92d54966172a9bf486
regisdantas <dantas.regis.m@gmail.com>
2022-03-12 11:54:20 -0300

    Added a map

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -6,16 +6,24 @@ const lines = 80;
 const cols = 134;
 const fps = 60;
 
+const mapLines = 20;
+const mapCols = 20;
+
 export default class Game extends Component {
   static propTypes = {};
   constructor(props) {
     super(props);
-    this.state = {
-      engine: new ASCIIGameEngine(lines, cols, fps),
-    };
+    this.engine = new ASCIIGameEngine(cols, lines, fps);
+    this.map = ASCIIGameEngine.CreateGameMap(mapCols, mapLines, {
+      wallChar: "#",
+      floorChar: ".",
+    });
+    this.state = {};
+    console.log(this.state.map);
   }
 
   render() {
-    return this.state.engine.render();
+    this.engine.DrawMap(this.map, 2, 2);
+    return this.engine.render();
   }
 }
--- src/components/react-ascii-game-engine/index.jsx
+++ src/components/react-ascii-game-engine/index.jsx
@@ -1,25 +1,63 @@
 import "./index.css";
 
 export default class ASCIIGameEngine {
-  constructor(lines, cols, fps) {
-    this.screenBuffer = new Array(lines).fill(
-        new Array(cols).fill('').map((_, idx) => `X`)
-      );
+  constructor(width, height, fps) {
+    this.screenBuffer = new Array(height + 2)
+      .fill()
+      .map((_, hidx) => new Array(width + 2).fill().map((_, widx) => " "));
+    this.screenBuffer[0][0] = "┌";
+    this.screenBuffer[0][width + 1] = "┐";
+    this.screenBuffer[height + 1][0] = "└";
+    this.screenBuffer[height + 1][width + 1] = "┘";
+    for (let i = 1; i <= width; i++) {
+      this.screenBuffer[0][i] = "─";
+      this.screenBuffer[height + 1][i] = "─";
+    }
+    for (let i = 1; i <= height; i++) {
+      this.screenBuffer[i][0] = "│";
+      this.screenBuffer[i][width + 1] = "│";
+    }
   }
 
   Draw(x, y, char) {
-    this.screenBuffer[y][x] = char;
+    this.screenBuffer[y + 1][x + 1] = char;
+  }
+
+  DrawMap(map, posx, posy) {
+    console.log(map)
+    map.forEach((line, y) => {
+      line.forEach((col, x) => {
+        this.Draw(x + posx, y + posy, map[y][x]);
+      });
+    });
   }
 
   render() {
     return (
       <div className="game-screen">
-        <p>
+        <pre>
           {this.screenBuffer.map((line) => {
-            return <span>{line}<br/></span>
+            return (
+              <span>
+                {line}
+                <br />
+              </span>
+            );
           })}
-        </p>
+        </pre>
       </div>
     );
+  }
+
+  static CreateGameMap(width, height, options) {
+    let map = new Array(height).fill().map((_, hidx) => 
+      new Array(width).fill().map((_, widx) => {
+        let char = options.floorChar ? options.floorChar : ".";
+        if (widx === 0 || widx === width - 1 || hidx === 0 || hidx === height - 1) {
+          char = options.wallChar ? options.wallChar : "#";
+        }
+        return char;
+      }));
+    return map;
   }
 }
